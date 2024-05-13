b9d4be69ae16ec6276d95003e6e4ce838585aded
tim <timdesmet00@gmail.com>
2024-05-13 10:14:42 +0200

    premier commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const connectdb = require("./config/db");
+const dotenv = require("dotenv").config();
+
+const port = 5000;
+
+connectdb();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/pokemons", require("./routes/pokemons.routes"));
+app.use("/users", require("./routes/users.routes"));
+
+app.listen(port, () => {
+  console.log(`Le server est démarré sur le port: ${port}`);
+});
