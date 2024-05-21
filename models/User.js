ded28c2af15861e7074246cf9d27df87378898fd
tim <timdesmet00@gmail.com>
2024-05-21 13:24:32 +0200

    mise en place des documents event

--- models/User.js
+++ models/User.js
@@ -24,6 +24,10 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
   },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 UserSchema.pre("save", async function (next) {
